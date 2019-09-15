41751eb75dcb019330cde532b6d3119ad2600434
Burak <97burakfidan97@gmail.com>
2019-09-15 20:38:22 +0300

    TODO's finished.

--- app/src/main/java/com/mrntlu/mysubscriptionmanager/utils/AlertReceiver.kt
+++ app/src/main/java/com/mrntlu/mysubscriptionmanager/utils/AlertReceiver.kt
@@ -7,9 +7,6 @@ import java.util.*
 
 class AlertReceiver:BroadcastReceiver() {
 
-    /*Todo set logical id by intent
-    2nd phase cancel notification and get currently active notifications
-    */
     override fun onReceive(context: Context?, i: Intent?) {
         val message=i?.getStringExtra("message")
         val hashCode=i?.getIntExtra("hastCode",Random().nextInt(9999 - 1000) + 1000)
